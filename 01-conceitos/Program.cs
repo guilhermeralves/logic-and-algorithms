2f8e867eaa1a8584cfc5825c9260f41cfabcd754
Guilherme Rodrigues <r.a.guilherme12@gmail.com>
2024-11-12 16:26:18 -0300

    module first in C#

---
+++ 01-conceitos/Program.cs
@@ -0,0 +1,8 @@
+﻿//Para realizar comentários de apenas uma linha, voce pode usar duas barras sequencias
+
+//já para comentários de duas linhas ou mais usa-se a formatação /**/ e tudo que estiver entre os asteristicos será um comentário.
+
+
+//Para printar ou exibir informações e valores é usado o seguite comando:
+
+Console.WriteLine();
---
+++ 02-variaveis/Program.cs
@@ -0,0 +1,23 @@
+﻿//variable create
+
+//text
+string name = "Guilherme";
+
+//letter
+char first = 'g';
+
+//Integers numbers
+int age = 22;
+
+//real numbers
+double height = 1.75;
+
+//logic
+bool happy = true;
+
+//value print variable
+Console.WriteLine(name);
+Console.WriteLine(first);
+Console.WriteLine(age);
+Console.WriteLine(height);
+Console.WriteLine(happy);
