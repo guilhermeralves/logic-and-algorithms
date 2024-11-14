71fb73e8958aee579bbec9d045277ac1357f3dfd
Guilherme Rodrigues <r.a.guilherme12@gmail.com>
2024-11-14 14:58:36 -0300

    update module 06

--- 05-readline/Program.cs
+++ 05-readline/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿// o comando readline é usado como input de dados pelo usuário.
+
+Console.WriteLine("Qual é a minha cor favorita?\n");
+
+/* input de dados do usuario para obter a informação de qual cor é a favorita.
+    Observação: o ponto de interrogação é usado antes do nome da variavel para informar ao C# que ela pode receber um valor nulo caso o usuário não informe nada e prossiga. 
+*/
+
+string ?corFavorita = Console.ReadLine();
+
+Console.WriteLine($"\nA sua cor favorita é o {corFavorita}");
---
+++ 06-conversao/Program.cs
@@ -0,0 +1,20 @@
+﻿/*nesse modulo o exemplo acontece nas linhas 7 e 13, o tipo de variavel do numero01 e numero02 é do tipo Int
+ou seja o programa espera um dado do tipo numero inteiro, porem o retorno padrão da função Console.ReadLine é texto, então nesse casso é necessário realizar a conversão do dado para que o input do usuario seja transformado em tipo int e aceito pela variavel numero01 e numero02*/
+
+//obter primeiro numero
+
+Console.WriteLine("Informe o primeiro numero:\n");
+
+int numero01 = Convert.ToInt16(Console.ReadLine());
+
+//obter segundo numero
+
+Console.WriteLine("Informe o segundo numero:\n");
+
+int numero02 = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("");
+
+//realizar a soma
+
+Console.WriteLine($"\nA soma dos numeros é igual à: {numero01+numero02}");
