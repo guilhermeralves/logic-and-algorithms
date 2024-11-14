9a22d330f1d0f2f75f3c5b27031ebd40e93e3f9e
Guilherme Rodrigues <r.a.guilherme12@gmail.com>
2024-11-14 11:22:05 -0300

    update modules

---
+++ 04-concatenacao/Program.cs
@@ -0,0 +1,15 @@
+﻿// o sinal de + é usado no C# para concatenar informações
+
+//variaveis
+
+string name = "Guilherme";
+string lastName = " Rodrigues";
+string city = "Caçapava";
+string state = "-SP";
+string country = " Brazil";
+
+Console.WriteLine(name+lastName+" mora em "+city+state+" localizado no país "+country+"!!!\n");
+
+//interpolação de strings, a concatenação tambem pode ser feita utilizando o simbolo de $ e colocando as variaveis dentro de chaves. 
+
+Console.WriteLine($"Meu nome é {name}{lastName}!");
