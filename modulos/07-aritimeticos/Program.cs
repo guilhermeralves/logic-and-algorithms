2a96d03afb76ea39f0200ae4297d4ebcaadf4f89
Guilherme Rodrigues <r.a.guilherme12@gmail.com>
2024-11-18 14:06:42 -0300

    update module

---
+++ modulos/07-aritimeticos/Program.cs
@@ -0,0 +1,29 @@
+﻿//Dentro do programa, é possivel realizar calculos aritimeticos ao longo do desenvolvimento do programa
+
+int numero1 = 91;
+int numero2 = 5;
+
+//é possivel realizar varias operações manipulando dados numericos como nos exemplos a seguir:
+
+//soma
+Console.WriteLine(numero1 + numero2);
+
+//subtração
+Console.WriteLine(numero1 - numero2);
+
+//multiplicação
+Console.WriteLine(numero1 * numero2);
+
+//Divisão
+Console.WriteLine(numero1 / numero2);
+
+//modulo (resto da divisão)
+Console.WriteLine(numero1 % numero2);
+
+//incrementador
+numero2++;
+Console.WriteLine(numero2);
+
+//decrementador
+numero2--;
+Console.WriteLine(numero2);
